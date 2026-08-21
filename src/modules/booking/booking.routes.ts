241ec7334efdf6bee4a8971348069185e91d7bc3
codewitharafat1 <arafatnill2.0@gmail.com>
2026-08-21 21:13:00 +0600

    add some functions

--- src/modules/booking/booking.routes.ts
+++ src/modules/booking/booking.routes.ts
@@ -5,10 +5,10 @@ import { Role } from "../../generated/prisma/enums";
 
 const router = Router();
 
-router.post("/", auth(Role.CUSTOMER), bookingControllers.createBooking);
-router.get("/my-bookings", auth(Role.CUSTOMER), bookingControllers.getMyBookings);
-router.get("/technician-bookings", auth(Role.TECHNICIAN), bookingControllers.getTechnicianBookings);
-router.patch("/my-bookings/:bookingId/cancel", auth(Role.CUSTOMER), bookingControllers.cancelMyBooking);
-router.patch("/my-bookings/:bookingId/accept",auth(Role.TECHNICIAN),  bookingControllers.acceptBooking);
+router.post("/", auth(Role.CUSTOMER, Role.ADMIN), bookingControllers.createBooking);
+router.get("/my-bookings", auth(Role.CUSTOMER, Role.ADMIN), bookingControllers.getMyBookings);
+router.get("/technician-bookings", auth(Role.TECHNICIAN, Role.ADMIN), bookingControllers.getTechnicianBookings);
+router.patch("/my-bookings/:bookingId/cancel", auth(Role.CUSTOMER, Role.ADMIN), bookingControllers.cancelMyBooking);
+router.patch("/my-bookings/:bookingId/accept",auth(Role.TECHNICIAN, Role.ADMIN),  bookingControllers.acceptBooking);
 
 export const bookingRoutes = router;
